c30f06e71f96d2f3f910e0336b7425fd5d04a653
Gustavo Romero <gromero@br.ibm.com>
2016-01-16 19:34:51 -0200

    Find if target address is in between pages
    Find out (and mark with a 'o') if the address we are looking for is in
    between to allocated blocks, hence it refers toa an address not mapped
    yet.
    
    Improve somehow comments about this simple AWK program.

--- findout.sh
+++ findout.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+######################################
+## $1 PID or filename
+## $2 address to look for in mem-map
+######################################
 findout()
 {
- PID=$1
- SI_ADDR=$2
 
 if [[ $1 =~ ^[0-9]+$ ]] ; then
    PAGES="pmap -X $1"	#it's a PID, use pmap
@@ -11,10 +13,56 @@ else
    PAGES="cat $1"	#or a file...
 fi
 
-#echo $PID
-#echo $SI_ADDR
+SI_ADDR=$2
 
-$PAGES | head --lines=-2 | tail --lines=+3 | gawk -v si_addr=$SI_ADDR --non-decimal-data '{ start = ("0x"$1)+0; end = start + ($6*1024); if ( start <= si_addr && si_addr <= end ) { printf ("%x\t + %x\t ", start,end) } else { printf ("%x\t - %x\t ", start, end) } ;  print $2, $13; if ( $0 ~ /\[stack\]/ ) { stack_border_addr=start } } END { printf("STACK  @ 0x%x\n", stack_border_addr); printf("SIGNAL @ 0x%x\n", si_addr); printf("SIGNAL DISTANCE FROM STACK: %d byte(s)\n",stack_border_addr-si_addr) }'
+$PAGES | head --lines=-2 | tail --lines=+3 | gawk -v si_addr=$SI_ADDR --non-decimal-data \
+'
+
+BEGIN { last_end = 0; }
+
+{
+  # Count how many pages to compute end. Page unit is KiB.
+  start = ("0x"$1)+0; end = start + ($6*1024);
+
+  # See if the address we are looking for is out of an
+  # allocated block (between blocks). If it does, compute
+  # its relative distence from the page above and from page below.
+  if ( last_end < si_addr && si_addr < start ) {
+   print si_addr-last_end,"\t\t o",start-si_addr;
+  }
+
+  # If the address is inside an allocated block the start
+  # and the end of the block and mark this fact with a plus (+)
+  # signal.
+  if ( start <= si_addr && si_addr <= end ) {
+  printf ("%x\t + %x\t ", start,end);
+  } else {
+    printf ("%x\t - %x\t ", start, end);
+  }
+  print $2, $13;
+
+  # If the block is assigned to be the stack of the main
+  # process (parent ID), record its start address, since,
+  # as stack grows downwards, the start address is a "border".
+  if ( $0 ~ /\[stack\]/ ) {
+    stack_border_addr=start;
+  }
+
+  # Update the current end address to be used in the next iteration,
+  # as we need it to see if the address we are searching is in between
+  # blocks, i.e., out side an allocated address.
+  last_end = end;
 }
 
-findout $1 $2
+END {
+  # Print some nutritional facts, mainly how far the address provide is
+  # from the parent address stack. Generally, the address we are searching
+  # is were a SIGSEGV exception occurred, hence here I call it "SIGNAL".
+  printf("STACK  @ 0x%x\n", stack_border_addr);
+  printf("SIGNAL @ 0x%x\n", si_addr);
+  printf("SIGNAL DISTANCE FROM STACK: %d byte(s)\n",stack_border_addr-si_addr)}
+'
+
+}
+
+findout $1 $2 # PID|filename Address
