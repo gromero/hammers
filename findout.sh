41b2829c81c6bcfe845bbee050343454552a75f6
Gustavo Romero <gromero@br.ibm.com>
2016-01-18 07:08:47 -0600

    Print block size in an easy way

--- findout.sh
+++ findout.sh
@@ -17,12 +17,23 @@ SI_ADDR=$2
 
 $PAGES | head --lines=-2 | tail --lines=+3 | gawk -v si_addr=$SI_ADDR --non-decimal-data \
 '
+function print_page_size(s)
+{
+ if (s < 1024) {
+   ret = s" B ";
+ } else if (s < 1024*1024) {
+     ret = s/1024" KiB ";
+   } else {
+      ret = s/(1024*1024)" MiB ";
+    }
+ return ret;
+}
 
 BEGIN { last_end = 0; }
 
 {
   # Count how many pages to compute end. Page unit is KiB.
-  start = ("0x"$1)+0; end = start + ($6*1024);
+  start = ("0x"$1)+0; size = ($6*1024); end = start + size;
 
   # See if the address we are looking for is out of an
   # allocated block (between blocks). If it does, compute
@@ -35,11 +46,11 @@ BEGIN { last_end = 0; }
   # and the end of the block and mark this fact with a plus (+)
   # signal.
   if ( start <= si_addr && si_addr <= end ) {
-  printf ("%x\t + %x\t ", start,end);
+    printf ("%x\t + %x\t", start, end);
   } else {
-    printf ("%x\t - %x\t ", start, end);
+      printf ("%x\t - %x\t", start, end);
   }
-  print $2, $13;
+  print $2, $13, "=>", print_page_size(size);
 
   # If the block is assigned to be the stack of the main
   # process (parent ID), record its start address, since,
@@ -58,6 +69,7 @@ END {
   # Print some nutritional facts, mainly how far the address provide is
   # from the parent address stack. Generally, the address we are searching
   # is were a SIGSEGV exception occurred, hence here I call it "SIGNAL".
+  print "";
   printf("STACK  @ 0x%x\n", stack_border_addr);
   printf("SIGNAL @ 0x%x\n", si_addr);
   printf("SIGNAL DISTANCE FROM STACK: %d byte(s)\n",stack_border_addr-si_addr)}
